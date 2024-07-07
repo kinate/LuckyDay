7618c8e2a8cc54f965d2c5d13b39f8da602ff9cc
kinate <kinate82@gmail.com>
2024-07-07 13:11:39 +0300

    Removed unused codes of counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Draw from './components/Draw'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Draw />
