2b0caea6b56371214efcb35a7a8870d2489489d4
kinate <kinate82@gmail.com>
2024-07-08 09:51:48 +0300

    Fix: Remove black background color

--- src/components/Draw.tsx
+++ src/components/Draw.tsx
@@ -31,44 +31,60 @@ export default function Draw() {
       console.log(typeof Number(inputValue));
     } else {
       setIsWinner(false);
-      SetTryCounter(tryCounter-1);
+      SetTryCounter(tryCounter - 1);
       console.log(luckyNumber);
     }
   }
 
   return (
     <>
-    <div className="container vh-100 d-flex justify-content-center align-items-center">
-      <div className="row w-100">
-        <div className="col-12 col-md-4 mx-auto" >
-          <div className="card p-4 text-center" style={{background:'#000',color:'#fff'}}>
-      <h1 style={{ 
-      backgroundColor: 'white', 
-      borderRadius: '40%', 
-      width: '60px', 
-      height: '60px', 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      margin: '0 auto' 
-    }}>{isWinner ? "🎉" : "😎"}</h1>
-      <h4> Pick a number 0 to 10 </h4>
+      <div className="container vh-100 d-flex justify-content-center align-items-center">
+        <div className="row w-100">
+          <div className="col-12 col-md-4 mx-auto">
+            <div
+              className="card p-4 text-center"
+              style={{ background: "#F8F8F8"}}
+            >
+              <h1
+                style={{
+                  backgroundColor: "white",
+                  borderRadius: "40%",
+                  width: "60px",
+                  height: "60px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  margin: "0 auto",
+                }}
+              >
+                {isWinner ? "🎉" : "😎"}
+              </h1>
+              <h4> Pick a number 0 to 10 </h4>
 
-      <p>
-        You have <b>{tryCounter}</b> chances to try
-      </p>
-      <div>
-        <input className="form-control" type="tel" value={inputValue} onChange={handleInput} />{" "}
-        {/* BUG onChange doesnt work propery*/}
-      </div>
-      <br />
-      <button className="btn btn-primary" disabled={isNumberAboveTen} onClick={handleEvent}>
-        {isNumberAboveTen ? "Hey! just 1-10 😡" : "Try my lucky"}
-      </button>
-      </div>
-      <p style={{color:'#ccc'}}>@2024 Luck Day</p>
-      </div>
-      </div>
+              <p>
+                You have <b>{tryCounter}</b> chances to try
+              </p>
+              <div>
+                <input
+                  className="form-control"
+                  type="tel"
+                  value={inputValue}
+                  onChange={handleInput}
+                />{" "}
+                {/* BUG onChange doesnt work propery*/}
+              </div>
+              <br />
+              <button
+                className="btn btn-primary"
+                disabled={isNumberAboveTen}
+                onClick={handleEvent}
+              >
+                {isNumberAboveTen ? "Hey! just 1-10 😡" : "Try my lucky"}
+              </button>
+            </div>
+            <p style={{ color: "#ccc" }}>@2024 Luck Day</p>
+          </div>
+        </div>
       </div>
     </>
   );
